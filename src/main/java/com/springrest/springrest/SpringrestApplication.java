8db3ec583de1066b2d9316e082ad23901e6bc7a1
pritam1293 <beherapritam2003@gmail.com>
2025-02-02 21:42:19 +0530

    Deleted the unnecessary classes

--- src/main/java/com/springrest/springrest/SpringrestApplication.java
+++ src/main/java/com/springrest/springrest/SpringrestApplication.java
@@ -1,7 +1,5 @@
 package com.springrest.springrest;
 
-import com.springrest.springrest.controller.A;
-import com.springrest.springrest.controller.B;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
